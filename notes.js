14cdd95db2752c516e1982efa2ff368c33ffe9f8
sujay2306 <sujayks007@gmail.com>
2021-09-30 20:38:37 +0530

    final

--- notes.js
+++ notes.js
@@ -3,23 +3,23 @@ const chalk = require("chalk")
 
 const getNotes = () => {
   return "Your notes..."
-};
+}
 
 const addNote = (title, body) => {
   const notes = loadNotes()
   const duplicateNote = notes.find((note)=>note.title === title)
-
+  debugger
   if (!duplicateNote) {
     notes.push({
       title: title,
       body: body,
-    });
+    })
     saveNotes(notes);
     console.log(chalk.green.inverse("New note added!"))
   } else {
     console.log(chalk.red.inverse("Note title taken!"))
   }
-};
+}
 
 const removeNote = (title) => {
   const notes = loadNotes()
@@ -33,7 +33,7 @@ const removeNote = (title) => {
   }
 }
 const listNotes = () => {
-  const notes = loadNotes();
+  const notes = loadNotes() 
 
   console.log(chalk.yellow.inverse("Your Notes"))
 
